58c13e461478970964973829a1fe294aa90394db
Bastian Alzate <44325651+bastianalzate@users.noreply.github.com>
2021-11-22 21:55:30 -0500

    -Agrego estilos al Navbarlatera -Agrego iconos al NavbarLateral

---
+++ src/data/socialData.js
@@ -0,0 +1,16 @@
+import linkedin from "../assets/icons/linkedin.png"
+import github from "../assets/icons/github.png"
+
+const social = [
+    {
+        icon: linkedin
+    },
+    {
+        icon: github
+    },
+    
+]
+
+export {
+    social
+} 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavbarLateral from "../../components/Navbar/Navbar";
+
+const Home = () => {
+    return(
+        <div>
+            <div className="w-1/5 h-screen border-2">
+                <NavbarLateral />
+            </div>
+            <div className="w-4/5">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home;
