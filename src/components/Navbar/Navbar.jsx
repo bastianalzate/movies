29843e2fed5a23c957c4e72b0687030e9534ee81
Bastian Alzate <44325651+bastianalzate@users.noreply.github.com>
2021-11-23 10:56:37 -0500

    -modifico un poco de estilos en el navbarLateral

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,38 +4,38 @@ import { social } from "../../data/socialData";
 const NavbarLateral = () => {
     return(
         <div className="w-full h-full">
-            <div className="flex w-full h-20 justify-items-start px-10 items-center">
+            <div className="flex w-full justify-items-start px-6 items-center h-1/5 border-2">
                 <span className="text-2xl font-bold">Nombre</span>
             </div>
 
-            <div className="flex flex-col w-full h-auto py-5 my-3">
-                <div className="w-full flex justify-items-start px-10"><span className="text-sm font-semibold text-gray-400">Menu</span></div>
-                <div className="w-full py-2 flex justify-items-start px-10">
+            <div className="flex flex-col w-full  h-1/5 border-2 py-2">
+                <div className="w-full flex justify-items-start px-6"><span className="text-sm font-semibold text-gray-400">Menu</span></div>
+                <div className="w-full py-2 flex justify-items-start px-6">
                     <span className="text-lg font-semibold text-gray-800">Browse</span>
                 </div>
-                <div className="w-full py-2 flex justify-items-start px-10">
+                <div className="w-full py-2 flex justify-items-start px-6">
                     <span className="text-lg font-semibold text-gray-800">Favoritos</span>
                 </div>
             </div>
 
-            <div className="flex flex-col w-full h-auto py-5 my-3">
-                <div className="w-full flex justify-items-start px-10"><span className="text-sm font-semibold text-gray-400">Social</span></div>
-                <div className="w-full py-2 flex justify-items-start px-10">
+            <div className="flex flex-col w-full  h-1/5 border-2 py-2">
+                <div className="w-full flex justify-items-start px-6"><span className="text-sm font-semibold text-gray-400">Social</span></div>
+                <div className="w-full py-2 flex justify-items-start px-6">
                     <span className="text-lg font-semibold text-gray-800">Friends</span>
                 </div>
             </div>
 
-            <div className="flex flex-col w-full h-auto py-5 my-3">
-                <div className="w-full flex justify-items-start px-10"><span className="text-sm font-semibold text-gray-400">General</span></div>
-                <div className="w-full py-2 flex justify-items-start px-10">
+            <div className="flex flex-col w-full  h-1/5 border-2 py-2">
+                <div className="w-full flex justify-items-start px-6"><span className="text-sm font-semibold text-gray-400">General</span></div>
+                <div className="w-full py-2 flex justify-items-start px-6">
                     <span className="text-lg font-semibold text-gray-800">Settings</span>
                 </div>
-                <div className="w-full py-2 flex justify-items-start px-10">
+                <div className="w-full py-2 flex justify-items-start px-6">
                     <span className="text-lg font-semibold text-gray-800">Logout</span>
                 </div>
             </div>
 
-            <div className="w-full h-10 flex justify-center items-center mt-14">
+            <div className="w-full  h-1/5 border-2 flex justify-center items-center ">
                 {
                     social?.map(icon => <img src={icon.icon} alt="" className="w-8 h-8 mx-2"/>)
                 }
