f94bad5120f8edfa964ad3d9458f24eccf400cf5
Bastian Alzate <44325651+bastianalzate@users.noreply.github.com>
2021-11-22 21:25:38 -0500

    -creo componente navbarLateral -creo un home en el cual hare los primeros renderizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
 import { useSelector } from 'react-redux';
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+
 
 function App() {
   const { saludo } = useSelector(state => state)
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-         {saludo}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/home" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
