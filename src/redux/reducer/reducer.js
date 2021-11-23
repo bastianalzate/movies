3db0c34b56ac1fa19c0e546e692145e31b0b7c39
Bastian Alzate <44325651+bastianalzate@users.noreply.github.com>
2021-11-22 20:47:04 -0500

    -agrego configuracion del store -agrego configuracion de reducer

---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    saludo: "sebastian"
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+
+
+        default:
+            return state;
+    }
+}
+
+export default reducer;
